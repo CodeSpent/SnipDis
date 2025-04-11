f3e184bd0849044a2e7bf488742d470ec49ce5c8
CodeSpent <hanfordpatrick@gmail.com>
2025-04-11 16:55:33 -0400

    feature: add sentry for telemetry

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import asyncio
 from bot import bot
+import sentry_sdk
+import os
+
+
 
 def main():
     """
     Entry point for starting the bot.
     """
     try:
+        sentry_sdk.init(
+            dsn=os.getenv("SENTRY_DSN"),
+            send_default_pii=True,
+        )
+
         asyncio.run(bot.run())
     except Exception as e:
         print(f"Error starting the bot: {e}")
